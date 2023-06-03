35033c07ea29a20da80a1f8b56cb577cdae1b562
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2023-06-03 22:06:55 +0700

    Initial Commit

---
+++ ExploreAbilityRelease/ExploreAbilityReleaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExploreAbilityReleaseApp.swift
+//  ExploreAbilityRelease
+//
+//  Created by Jia Chen Yee on 3/6/23.
+//
+
+import SwiftUI
+
+@main
+struct ExploreAbilityReleaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
