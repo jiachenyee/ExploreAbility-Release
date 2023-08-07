476dd3bac67662bebc5d038c703f813eb486a18f
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2023-08-08 07:10:21 +0800

    add mono audio validation

--- ExploreAbilityRelease/Views/Challenge/Challenges/Mono Audio/MonoAudioChallengeView.swift
+++ ExploreAbilityRelease/Views/Challenge/Challenges/Mono Audio/MonoAudioChallengeView.swift
@@ -14,47 +14,75 @@ struct MonoAudioChallengeView: View {
     
     private let publisher = NotificationCenter.default.publisher(for: UIAccessibility.monoAudioStatusDidChangeNotification)
     
+    @State private var isMonoAudioEnabled = UIAccessibility.isMonoAudioEnabled
+    @State private var initialIsMonoAudioEnabled = false
+    
     @EnvironmentObject var challengeViewModel: ChallengeViewModel
     @EnvironmentObject var viewModel: ViewModel
     
     @State private var leftCircleProgress = 0.0
     @State private var rightCircleProgress = 0.0
     
+    @State private var didFinishChallenge = false
+    
     @StateObject private var audioViewModel = MonoAudioChallengeAudioViewModel()
     
     private let timer = Timer.publish(every: 1, on: .current, in: .common).autoconnect()
     
     var body: some View {
-        GeometryReader { proxy in
-            Text("Put on your headphones.")
-                .padding()
-                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
-            Circle()
-                .fill(challengeViewModel.challenge.color.opacity(0.5 - 0.5 * leftCircleProgress))
-                .frame(width: proxy.size.width * (leftCircleProgress * 2), height: proxy.size.width * (leftCircleProgress * 2))
-                .offset(x: -(proxy.size.width * (leftCircleProgress * 2)) / 2)
-                .frame(maxHeight: .infinity, alignment: .center)
-            
-            Circle()
-                .fill(challengeViewModel.challenge.color.opacity(0.5 - 0.5 * rightCircleProgress))
-                .frame(width: proxy.size.width * (rightCircleProgress * 2), height: proxy.size.width * (rightCircleProgress * 2))
-                .offset(x: proxy.size.width - (proxy.size.width * (rightCircleProgress * 2)) / 2)
-                .frame(maxHeight: .infinity, alignment: .center)
-            
-            ChallengeHomeButton()
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
-        }
-        .onAppear {
-            playLeft()
-        }
-        .onReceive(timer) { _ in
-            if Bool.random() {
-                if leftCircleProgress == 0 {
+        Group {
+            if !isFeatureToggled {
+                GeometryReader { proxy in
+                    Text("Put on your headphones.")
+                        .padding()
+                        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+                    Circle()
+                        .fill(challengeViewModel.challenge.color.opacity(0.5 - 0.5 * leftCircleProgress))
+                        .frame(width: proxy.size.width * (leftCircleProgress * 2), height: proxy.size.width * (leftCircleProgress * 2))
+                        .offset(x: -(proxy.size.width * (leftCircleProgress * 2)) / 2)
+                        .frame(maxHeight: .infinity, alignment: .center)
+                    
+                    Circle()
+                        .fill(challengeViewModel.challenge.color.opacity(0.5 - 0.5 * rightCircleProgress))
+                        .frame(width: proxy.size.width * (rightCircleProgress * 2), height: proxy.size.width * (rightCircleProgress * 2))
+                        .offset(x: proxy.size.width - (proxy.size.width * (rightCircleProgress * 2)) / 2)
+                        .frame(maxHeight: .infinity, alignment: .center)
+                    
+                    ChallengeHomeButton()
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                }
+                .onAppear {
                     playLeft()
-                } else if rightCircleProgress == 0 {
-                    playRight()
+                    initialIsMonoAudioEnabled = isMonoAudioEnabled
+                }
+                .onReceive(timer) { _ in
+                    if Bool.random() {
+                        if leftCircleProgress == 0 {
+                            playLeft()
+                        } else if rightCircleProgress == 0 {
+                            playRight()
+                        }
+                    }
+                }
+                .onChange(of: isMonoAudioEnabled) { newValue in
+                    guard newValue != initialIsMonoAudioEnabled else { return }
+                    
+                    withAnimation {
+                        challengeViewModel.state = .playing(true)
+                    }
+                }
+            } else {
+                PlayingFeatureOnView(initialState: initialIsMonoAudioEnabled,
+                                     didSucceed: $didFinishChallenge)
+                .onChange(of: isMonoAudioEnabled) { newValue in
+                    if initialIsMonoAudioEnabled == newValue {
+                        didFinishChallenge = true
+                    }
                 }
             }
+        }
+        .onReceive(publisher) { _ in
+            isMonoAudioEnabled = UIAccessibility.isMonoAudioEnabled
         }
     }
     
