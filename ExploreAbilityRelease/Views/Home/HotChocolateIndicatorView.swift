9f0642efcc081f07a02436c8c4099f124cd9fa69
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2023-08-11 02:19:54 +0800

    make hot chocolate indicator a button that brings up the cafe view

--- ExploreAbilityRelease/Views/Home/HotChocolateIndicatorView.swift
+++ ExploreAbilityRelease/Views/Home/HotChocolateIndicatorView.swift
@@ -12,27 +12,37 @@ struct HotChocolateIndicatorView: View {
     
     @AppStorage("hotChocolates") private var hotChocolates = 3
     @State private var mugAnimationAngle = Angle.degrees(0)
+    @State private var isCafePresented = false
     
     var body: some View {
-        HStack {
-            Text("\(hotChocolates)")
-            ZStack {
-                Image(systemName: "mug.fill")
-                
-                Image(systemName: "mug.fill")
-                    .foregroundStyle(.brown)
-                    .clipShape(WaveView(offset: mugAnimationAngle, percent: 0.5))
-                    .onAppear {
-                        withAnimation(.linear(duration: 3).repeatForever(autoreverses: false)) {
-                            mugAnimationAngle = .degrees(360)
+        Button {
+            isCafePresented.toggle()
+        } label: {
+            HStack {
+                Text("\(hotChocolates)")
+                ZStack {
+                    Image(systemName: "mug.fill")
+                    
+                    Image(systemName: "mug.fill")
+                        .foregroundStyle(.brown)
+                        .clipShape(WaveView(offset: mugAnimationAngle, percent: 0.5))
+                        .onAppear {
+                            withAnimation(.linear(duration: 3).repeatForever(autoreverses: false)) {
+                                mugAnimationAngle = .degrees(360)
+                            }
                         }
-                    }
+                }
             }
+            .foregroundStyle(.white)
+            .padding()
+            .background(.ultraThickMaterial)
+            .clipShape(RoundedRectangle(cornerRadius: 8))
         }
-        .padding()
-        .background(.ultraThickMaterial)
-        .clipShape(RoundedRectangle(cornerRadius: 8))
         .padding([.trailing, .top])
+        .sheet(isPresented: $isCafePresented) {
+            RewardCafeView()
+                .presentationDetents([.medium, .large])
+        }
         .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topTrailing)
     }
 }
